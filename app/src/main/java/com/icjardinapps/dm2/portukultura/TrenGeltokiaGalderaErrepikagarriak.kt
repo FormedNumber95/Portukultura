4115bc95850a929671be3e57b3ff3dd742e26ea3
FormedNumber95 <aketza200510@gmail.com>
2024-12-23 17:01:46 +0100

    añadida la apariencia de la primera parte de las preguntas del ejercicio3

---
+++ app/src/main/java/com/icjardinapps/dm2/portukultura/TrenGeltokiaGalderaErrepikagarriak.kt
@@ -0,0 +1,14 @@
+package com.icjardinapps.dm2.portukultura
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TrenGeltokiaGalderaErrepikagarriak : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tren_geltokia_galdera_errepikagarriak)
+    }
+}
