c73a683b59835a26eb4aea3b4871396739c12a47
FormedNumber95 <aketza200510@gmail.com>
2024-12-26 12:19:17 +0100

    añadida la parte grafica de la actividad de la cancion

--- app/src/main/java/com/icjardinapps/dm2/portukultura/Mapa.kt
+++ app/src/main/java/com/icjardinapps/dm2/portukultura/Mapa.kt
@@ -91,7 +91,7 @@ class Mapa : AppCompatActivity(), OnMapReadyCallback {
         // Mapa de actividades para los marcadores
         markerActivities = markersList.mapIndexed { index, marker ->
             val activity = when (index) {
-                0 -> SopaDeLetras::class.java
+                0 -> Abestia::class.java
                 1 -> Puzzle::class.java
                 2 -> SopaDeLetras::class.java
                 3 -> SopaDeLetras::class.java
