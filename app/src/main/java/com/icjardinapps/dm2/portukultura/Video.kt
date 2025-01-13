ec986d306d21dd4e6f0f93f0f9f94057b6f0bdb0
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2025-01-13 12:25:38 +0100

    añadidos los videos de las actividades
    el tiempo de build y de launch es de 1min 30s aprox
    el viedo de la actividad 3 pesa mucho como para subirlo a git

--- app/src/main/java/com/icjardinapps/dm2/portukultura/Video.kt
+++ app/src/main/java/com/icjardinapps/dm2/portukultura/Video.kt
@@ -3,13 +3,16 @@ package com.icjardinapps.dm2.portukultura
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.MediaController
 import android.widget.TextView
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
 class Video : AppCompatActivity() {
 
     private lateinit var timerController: TimerController
     private lateinit var btnAurrera: Button
+    private lateinit var videoView:VideoView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +21,7 @@ class Video : AppCompatActivity() {
         val txtTiempo: TextView = findViewById(R.id.txtTiempo)
         btnAurrera = findViewById(R.id.btnAurrera)
         btnAurrera.isEnabled = false
+        videoView=findViewById(R.id.videoView)
 
         // Obtener el índice del marcador
         val markerIndex = intent.getIntExtra("MARKER_INDEX", -1)
@@ -41,9 +45,24 @@ class Video : AppCompatActivity() {
     }
 
     /**
-     * Devuelve la configuración del temporizador y acción del botón según el índice del marcador.
+     * Devuelve la configuración del temporizador, la acción del botón y el video según el índice del marcador.
      */
     private fun getTimerConfig(markerIndex: Int): TimerConfig {
+        val videoResId = when (markerIndex) {
+            0 -> R.raw.video_act1 // Video para Actividad 1
+            1 -> R.raw.video_act2 // Video para Actividad 2
+            2 -> R.raw.video_act3 // Video para Actividad 3
+            4 -> R.raw.video_act5 // Video para Actividad 5
+            else -> R.raw.video_act7   // Video por defecto para otras actividades
+        }
+
+        // Configurar el video en el VideoView
+        val videoUri = "android.resource://$packageName/$videoResId"
+        videoView.setVideoPath(videoUri)
+        val mediaController=MediaController(this)
+        videoView.setMediaController(mediaController)
+        videoView.start()
+
         return when (markerIndex) {
             //Actividad 1
             0 -> TimerConfig(
